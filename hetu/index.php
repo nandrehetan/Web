dd5d22f9ed91a8e9a5aa0665ed9e90aebb28c7e7
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-01-07 10:17:13 +0530

    Add files via upload

---
+++ hetu/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+In PHP, a variable starts with the $ sign, followed by the name of the variable:
+Rules for PHP variables:
+
+        A variable starts with the $ sign, followed by the name of the variable
+        A variable name must start with a letter or the underscore character
+        A variable name cannot start with a number
+        A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+        Variable names are case-sensitive ($age and $AGE are two different variables)
+*/
+
+$name = "Hetan";
+echo "I love $name <br>"; 
+
+$x=5;
+$y= 6;
+echo"Sum of $x and $y is ", $x+$y, "<br";
+
+
+
+?>
