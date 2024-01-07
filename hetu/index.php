75cd747dfed709953dbd36247ed9e0eae71db0d9
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-01-07 11:52:24 +0530

    Add files via upload

---
+++ hetu/index.php
@@ -0,0 +1,19 @@
+<?php
+
+function myMessage() {
+        echo "Hello world!";
+      }
+      
+myMessage();
+
+echo"<br>";
+
+function familyName($fname, $year) {
+        echo "$fname Refsnes. Born in $year <br>";
+}
+      
+familyName("Hege", "1975");
+familyName("Stale", "1978");
+familyName("Kai Jim", "1983");
+
+?>
