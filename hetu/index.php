16465c6b28c1ae19aeafb03a81bec74158b42d3e
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-01-07 11:20:50 +0530

    Add files via upload

---
+++ hetu/index.php
@@ -0,0 +1,29 @@
+<!-- 
+PHP divides the operators in the following groups:
+
+        Arithmetic operators
+        Assignment operators
+        Comparison operators
+        Increment/Decrement operators
+        Logical operators
+        String operators
+        Array operators
+        Conditional assignment operators
+-->
+<?php
+
+$a=45;
+$b=23;
+
+echo $a+$b;
+echo"<br>";
+echo $a-$b;
+echo"<br>";
+echo $a*$b;
+echo"<br>";
+echo $a/$b;
+echo"<br>";      
+
+
+
+?>
