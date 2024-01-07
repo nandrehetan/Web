6920d80de9f54fed6fec4cea2a1ccefbcc011d58
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-01-07 10:42:53 +0530

    Add files via upload

---
+++ hetu/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+PHP supports the following data types:
+
+        String
+        Integer
+        Float (floating point numbers - also called double)
+        Boolean
+        Array
+        Object
+        NULL
+        Resource
+
+You can get the data type of ant object by using the var_dump() function
+
+*/
+
+$name = "Hetan Hemant Nandre";
+$income = 100000;
+
+var_dump($income);
+
+
+?>
